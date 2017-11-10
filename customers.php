6b45729c07feaeba7569efd9056aa022ada24806
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-11-10 10:38:26 +0100

    #3: Solution

--- customers.php
+++ customers.php
@@ -16,20 +16,28 @@ try {
     $statement = $pdo->prepare($sql);
     $statement->execute();
     $customers = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $address_stmnt = $pdo->prepare("SELECT * FROM customer_addresses ORDER BY customer_id");
-    $address_stmnt->execute();
-    $addresses = $address_stmnt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($customers as $key => $customer) {
-        $address = array_filter($addresses, function($address) use ($customer) {
-            return ($address['customer_id'] == $customer['id']);
-        });
-        $address = (count($address) > 0) ? array_shift($address) : null;
-        // array_pop
-        if ($address != null) {
-            $customers[$key]['address'] = $address;
+
+    if (count($customers)>0) {
+        $address_stmnt = $pdo->prepare("SELECT * FROM customer_addresses ORDER BY customer_id");
+        $address_stmnt->execute();
+        $addresses = $address_stmnt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($customers as $key => $customer) {
+            $address = array_filter($addresses, function($address) use ($customer) {
+                return ($address['customer_id'] == $customer['id']);
+            });
+            $address = (count($address) > 0) ? array_shift($address) : null;
+            // array_pop
+            if ($address != null) {
+                $customers[$key]['address'] = $address;
+            }
         }
+        echo json_encode($customers);
     }
-    echo json_encode($customers);
+    else {
+        header("HTTP/1.0 404 Not Found");
+        echo "{\"message\": \"Customer not found\"}";
+    }
+
 }
 
 catch (PDOException $e) {
