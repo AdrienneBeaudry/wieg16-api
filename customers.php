0fc947bb4e29924dcf4e81f5e6efa6ec866407a0
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-11-21 10:45:36 +0100

    #4: Solution point 4

--- customers.php
+++ customers.php
@@ -4,12 +4,10 @@ require 'functions.php';
 require 'config.php';
 $code = 200;
 $response = [];
-$get_customer_id = $_GET['customer_id'];
+$get_customer_id = isset($_GET['customer_id']) ? $_GET['customer_id'] : null;
+$show_address_only = isset($_GET['address']) ? $_GET['address'] : null;
 
-if (isset($_GET['customer_id'])&&($_GET['address'])) {
-    $sql = "SELECT customer_addresses.* FROM customer_addresses WHERE customer_id=$get_customer_id";
-}
-elseif (isset($_GET['customer_id'])) {
+if (isset($_GET['customer_id'])) {
     $sql = "SELECT customers.* FROM customers WHERE id=$get_customer_id";
 }
 else {
@@ -26,37 +24,38 @@ try {
         $address_stmnt->execute();
         $addresses = $address_stmnt->fetchAll();
 
-        if (isset($_GET['address']) and isset($_GET['customer_id'])) {
-
-        } else {
-
-        }
-
         foreach ($customers as $key => $customer) {
-            // WHY function /use syntax here. Don't get it...
+            // array_filter function parses through the array and only keeps the items that pass "the test"
+            // i.e. where the callback function returns true.
+            // it will return a new array with only the values that "passed"
+            // The syntax below uses an anonymous function, meaning that we are defining the function "$address"
+            // inside the curly brackets and we are also passing along with the function
+            // a variable found in the parent's scope with the particular syntax (use) needed for that.
             $address = array_filter($addresses, function ($address) use ($customer) {
                 return ($address['customer_id'] == $customer['id']);
             });
+            $customers[$key]['address'] = $address;
 
             // Note: the function array_filter will retain the original index for each values that "passes the filter"
             // or passes the test. In order to reset the index to 0, 1, 2, 3 we will need to use the function array_values.
             // $address = array_values($address);
+            
 
-            //WHY the below? Longer then the solution I would have done underneath??
-            $address = (count($address) > 0) ? array_shift($address) : null;
-            if ($address != null) {
-                // This embeds the child child in the last key of the main array
-                $customers[$key]['address'] = $address;
+            if ($show_address_only != null) {
+                if (count($address) > 0) {
+                    $response = $address;
+                }
+                else {
+                    $code = 404;
+                    $response = ['message' => 'Address not found'];
+                }
             }
-
-            if (count($address) > 0) {
-                $customers[$key]['address'] = array_shift($address);
+            else {
+                $response = $customers;
             }
-
-            /*
-*/
         }
-        $response = $customers;
+
+
     } else {
         $code = 404;
         $response = ['message' => 'Customer not found'];
